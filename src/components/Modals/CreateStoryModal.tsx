14e5235a811a5200a0c3a355d0dfd9bdf3c4884e
Dinesh <dlaller1988@gmail.com>
2024-12-13 18:28:03 +0530

    Schedule Post 11-12-2024

--- src/components/Modals/CreateStoryModal.tsx
+++ src/components/Modals/CreateStoryModal.tsx
@@ -5,15 +5,12 @@ import { RxCross2 } from "react-icons/rx";
 import Button from "../Buttons/Button";
 import { IoMdAdd, IoMdSend } from "react-icons/io";
 import { HiOutlineChevronUpDown } from "react-icons/hi2";
-import { toasterError, toasterInfo, toasterSuccess } from "../core/Toaster";
+import { toasterInfo } from "../core/Toaster";
 import { useApi } from "@/hooks/useAPI";
-import { uploadMultiFile } from "../core/UploadFile";
-import { useState } from "react";
-import { IoCloseCircle } from "react-icons/io5";
-import { useSelector } from "react-redux";
+import { uploadFile } from "../core/UploadFile";
 
 interface Story {
-  storyText: string;
+  storyLinkText: string;
   storyLink: string;
   storyMedia: File | null; // File or null to account for empty values
   duration: number;
@@ -23,21 +20,17 @@ interface Story {
 
 interface InitialValues {
   storyCoverImage: File | null;
-  coverTitle: string;
+  storyCoverTitle: string;
   stories: Story[];
 }
 const CreateStoryModal: React.FC<any> = ({ togglePopup, onAddStory }) => {
   const { API } = useApi();
-  const userId = useSelector((state: any) => state.auth.id);
-  const [filePreview, setFilePreview] = useState<string | null>(null);
-
   const initialValues = {
-    userId:userId,
     storyCoverImage: null,
-    coverTitle: "",
+    storyCoverTitle: "",
     stories: [
       {
-        storyText: "",
+        storyLinkText: "",
         storyLink: "",
         storyMedia: null,
         duration: 6,
@@ -47,67 +40,31 @@ const CreateStoryModal: React.FC<any> = ({ togglePopup, onAddStory }) => {
     ],
   };
 
-  const handleAddStory = async () => {
-      console.log(initialValues); // Debug values here
+  const handleAddStory = (values: any) => {
 
-    try {
-      const { success, error } = await API.post("story/create", {
-        initialValues
-      });
-      if (success) {
-
-        toasterSuccess("Add Story Created SuccessFully !", 2000, "id")
-      } else {
-        toasterError(error || "Failed to Create comment");
-      }
-    } catch (err) {
-      console.error("Error posting comment:", err);
-      toasterError("An error occurred while posting the comment");
-    }
+    const storyData = {
+      cover: initialValues.storyCoverImage,
+      title: initialValues.storyCoverTitle,
+      stories: initialValues.stories,
+    };
+    console.log(storyData)
+    onAddStory(storyData);
+    togglePopup();
   };
 
 
-  const handleFileChange = async (
-    e: React.ChangeEvent<HTMLInputElement>, 
-    setFieldValue: any, 
-    fieldName: string, 
-    setFilePreview: any,
-    setStoryMedia: any,
-    isCoverImage: boolean,  
-    storyIndex?: number  
-  ) => {
-    const files = e.target.files;
-    if (files && files.length > 0) {
-      const file = files[0];
-      
-      if (isCoverImage) {
-        setFieldValue(fieldName, file);
-        const reader = new FileReader();
-        reader.onload = (event) => {
-          setFilePreview(event.target?.result as string);
-        };
-        reader.readAsDataURL(file);
-      } else {
-        const newFiles = Array.from(files);
-        const uploadData = await uploadMultiFile(newFiles, API);  
-  
-        const updatedStoryMedia = { ...setStoryMedia };
-  
-        if (storyIndex !== undefined) {
-          updatedStoryMedia[storyIndex] = uploadData; 
-          setStoryMedia(updatedStoryMedia);
+  const handleChange=async(e:any ,setname:any,name:any)=>{
+    const file = e.target.files?.[0];
+    if (file) {
+        try {
+            const uploadedUrl = await uploadFile(file,API);
+            setname(name, uploadedUrl);
+            toasterInfo("File uploaded successfully");
+        } catch (error) {
+            console.error("Error uploading file:", error);
         }
-      }
     }
-  };
-
-  const handleDelete = (setFieldValue: any, fieldName: string) => {
-    setFilePreview(null); 
-    setFieldValue(fieldName, null); 
-  };
-
-
-
+  }
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
       <div className="max-h-[80vh] overflow-auto bg-[var(--sections)] border border-white/10 w-full max-w-[600px] rounded-[12px] shadow-lg">
@@ -129,61 +86,49 @@ const CreateStoryModal: React.FC<any> = ({ togglePopup, onAddStory }) => {
           onSubmit={handleAddStory}
         >
           {({ values, setFieldValue }) => (
+            console.log(values, "="),
             (
               <Form className="space-y-6 p-4">
                 <div className="">
                   <h3 className="text-center text-lg font-semibold mb-4 text-white">
                     Story Cover
                   </h3>
-                  <div className="p-5 border-2 border-dashed bg-gray-100/10 border-gray-300/20 rounded-full w-36 h-36 mx-auto flex items-center justify-center mb-4 relative">
-                    {filePreview ? (
-                      <div className="relative w-full h-full">
-                        <img
-                          src={filePreview}
-                          alt="Uploaded Preview"
-                          className="w-full h-full object-cover rounded-full"
-                        />
-                        {/* Cross Button */}
-                        <button
-                          type="button"
-                          onClick={() => handleDelete(setFieldValue, "storyCoverImage")}
-                          className="absolute top-0 right-0 bg-red-600 text-white rounded-full p-1 hover:bg-red-800"
-                          title="Remove Image"
-                        >
-                          <IoCloseCircle size={24} />
-                        </button>
-                      </div>
-                    ) : (
-                      <label
-                        htmlFor="storyCoverImage"
-                        className="text-center text-gray-500 cursor-pointer hover:text-green-600 flex flex-col items-center justify-center"
-                      >
-                        <p className="text-sm text-white">Drag & Drop your file</p>
-                        <p className="text-sm text-white">
-                          or <u className="text-white">Browse</u>
-                        </p>
-                        <Field
-                          type="file"
-                          name="storyCoverImage"
-                          id="storyCoverImage"
-                          className="hidden"
-                          onChange={(e:any) => handleFileChange(e, setFieldValue, "storyCoverImage", setFilePreview, setStoryMedia, true)}
-                          value={initialValues.storyCoverImage}
-                        />
-                      </label>
-                    )}
+                  <div className="p-5 border-2 border-dashed bg-gray-100/10 border-gray-300/20 rounded-full w-36 h-36 mx-auto flex items-center justify-center mb-4">
+                    <label
+                      htmlFor="storyCoverImage"
+                      className="text-center text-gray-500 cursor-pointer hover:text-green-600"
+                    >
+                      <p className="text-sm text-white">
+                        Drag & Drop your file
+                      </p>
+                      <p className="text-sm text-white">
+                        or{" "}
+                        <u className="text-white">Browse</u>
+                      </p>
+                      <Field
+                        type="file"
+                        name="storyCoverImage"
+                        id="storyCoverImage"
+                        className="hidden"
+                        // onChange={(e: any) =>
+                        //   setFieldValue("storyCoverImage", e.target.files[0])
+                        // }
+                        onChange={(e:any)=>handleChange(e,setFieldValue,"storyCoverImage")}
+                        value={initialValues.storyCoverImage}
+                      />
+                    </label>
                   </div>
                   <p className="text-center">Recommended sizes: 180x180 px.</p>
 
                   <div className="mt-4 flex flex-col gap-1">
                     <label
-                      htmlFor="coverTitle"
+                      htmlFor="storyCoverTitle"
                       className="block text-md font-medium text-white"
                     >
                       Story Cover Title
                     </label>
                     <InputField
-                      name="coverTitle"
+                      name="storyCoverTitle"
                       type="text"
                       placeholder="Enter Story Cover Title"
                     />
@@ -227,14 +172,14 @@ const CreateStoryModal: React.FC<any> = ({ togglePopup, onAddStory }) => {
 
                           <div className="mt-4 flex flex-col gap-1">
                             <label
-                              htmlFor={`stories[${index}].storyText`}
+                              htmlFor={`stories[${index}]?.storyLinkText`}
                               className="block text-md font-medium text-white"
                             >
                               Story Link Text
                             </label>
 
                             <InputField
-                              name={`stories[${index}].storyText`}
+                              name={`stories[${index}]?.storyLinkText`}
                               type="text"
                               placeholder="Enter link text"
                             />
@@ -264,58 +209,38 @@ const CreateStoryModal: React.FC<any> = ({ togglePopup, onAddStory }) => {
                                   Story Media (Image/Video)
                                 </label>
                                 <div className="">
-                                  <div className="p-6 rounded-xl border-dashed border-2 border-white/10 relative">
-                                    {filePreview ? (
-                                      <div className="relative">
-                                        {/* Inline Preview */}
-                                        {filePreview.startsWith("data:video/") ? (
-                                          <video
-                                            src={filePreview}
-                                            className="w-full h-64 rounded-xl object-cover"
-                                            controls
-                                          />
-                                        ) : (
-                                          <img
-                                            src={filePreview}
-                                            alt="Uploaded Preview"
-                                            className="w-full h-64 rounded-xl object-cover"
-                                          />
-                                        )}
-                                        {/* Cross Button */}
-                                        <button
-                                          type="button"
-                                          onClick={() => handleDelete(setFieldValue, `stories[${index}].storyMedia`)}
-                                          className="absolute top-2 right-2 bg-red-600 text-white rounded-full p-1 hover:bg-red-800"
-                                          title="Remove File"
-                                        >
-                                          <IoCloseCircle size={24} />
-                                        </button>
-                                      </div>
-                                    ) : (
-                                      <label
-                                        htmlFor={`file-upload-${index}`}
-                                        className="cursor-pointer py-4 px-6 rounded-md mt-4 flex flex-col gap-1 block text-center transition-all"
-                                      >
-                                        <span className="text-sm">Drag & Drop your file here or</span>
-                                        <span className="text-lg mt-2 ml-1 text-white underline">
-                                          Browse
-                                        </span>
-                                      </label>
-                                    )}
+                                  <div className="p-6 rounded-xl border-dashed border-2 border-white/10">
+                                    <label
+                                      htmlFor={`file-upload-${index}`}
+                                      className="cursor-pointer py-4 px-6 rounded-md mt-4 flex flex-col gap-1 block text-center transition-all "
+                                    >
+                                      <span className="text-sm ">
+                                        Drag & Drop your file here or
+                                      </span>
+                                      <span className="text-lg mt-2 ml-1 text-white underline">
+                                        Browse
+                                      </span>
+                                    </label>
                                   </div>
-                                  <p className="mt-2 text-sm">
-                                    Allowed types: .jpg, .jpeg, .png, .gif, .mp4, .mov, .wmv, .avi, .mpeg,
-                                    .3gp.
+                                  <p className=" mt-2 text-sm">
+                                    Allowed types: .jpg, .jpeg, .png, .gif,
+                                    .mp4, .mov, .wmv, .avi, .mpeg, .3gp.
                                   </p>
                                 </div>
                                 <Field
                                   type="file"
                                   name={`stories[${index}].storyMedia`}
-                                  onChange={(e:any) => handleFileChange(e, setFieldValue, `stories[${index}].storyMedia`, setFilePreview, setStoryMedia, false, index)}
-
-                                  value={initialValues?.stories[index]?.storyMedia}
-                                  id={`file-upload-${index}`}
-                                  className="hidden"
+                                  onChange={(e: any) =>
+                                    setFieldValue(
+                                      `stories[${index}].storyMedia`,
+                                      e.target.files[0]
+                                    )
+                                  }
+                                  value={
+                                    initialValues?.stories[index]?.storyMedia
+                                  }
+                                  id={`file-upload-${index}`} // Give the input a unique ID
+                                  className="hidden" // The input is hidden, but the label will open the file dialog
                                 />
                               </div>
 
@@ -363,7 +288,7 @@ const CreateStoryModal: React.FC<any> = ({ togglePopup, onAddStory }) => {
                           icon={<IoMdAdd className="fill-white" />}
                           onClick={() => {
                             push({
-                              storyText: "",
+                              storyLinkText: "",
                               storyLink: "",
                               storyMedia: null,
                               duration: 6,
